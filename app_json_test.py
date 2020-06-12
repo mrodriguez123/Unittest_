86d607ba529680e542eade0c19da94b565dda18d
mrodriguez123 <44066683+mrodriguez123@users.noreply.github.com>
2020-06-12 10:31:37 -0500

    Updated Requests and format

--- app_json_test.py
+++ app_json_test.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import unittest
-import pytest
-=======
-from flask import Flask
->>>>>>> 8eaa49a7c5dc7518789dae8d03b321d592a797f9
-import app_json
+import unittest, pytest
 from flask import Flask, request
-
+import app_json
 
 dictionary= { "name": " Monica",
     	"school": " University of Wisconsin",
