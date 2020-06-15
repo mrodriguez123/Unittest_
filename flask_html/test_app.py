923ec03a9196389d11f4a1c1fad825306e3fb6a6
mrodriguez123 <44066683+mrodriguez123@users.noreply.github.com>
2020-06-15 11:06:02 -0500

    Update test_app.py

--- flask_html/test_app.py
+++ flask_html/test_app.py
@@ -1,13 +1,6 @@
 import unittest
 import app
-
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
-    
+import urllib.request as urllib2
 from flask import Flask
 from flask_testing import TestCase, LiveServerTestCase
 
