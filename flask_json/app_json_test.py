6cd656f4e758b3d9b583211fd80d95da8926fe74
mrodriguez123 <44066683+mrodriguez123@users.noreply.github.com>
2020-06-15 11:08:59 -0500

    Update app_json_test.py

--- flask_json/app_json_test.py
+++ flask_json/app_json_test.py
@@ -1,6 +1,7 @@
 import unittest, pytest
 from flask import Flask, request
 import app_json
+import urllib.request as urllib2
 
 dictionary= { "name": " Monica",
     	"school": " University of Wisconsin",
